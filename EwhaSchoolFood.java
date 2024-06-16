0709b3e1e5fd43194a59019e174fc6dacc7d8a3d
aau0n <aau0n@ewha.ac.kr>
2024-06-16 20:14:42 +0900

    Add files via upload

--- EwhaSchoolFood.java
+++ EwhaSchoolFood.java
@@ -1,5 +1,4 @@
-//사진 경로는 C:/EwhaKiosk라서 EwhKiosk로 폴더 하나 만들고 사진 넣어주세요! 너무 지저분하게 업로드됐네요 ㅎㅎ ㅠㅠ
-
+//+ 3열로 만들거나, 3열로 만들기 어려우면 메뉴 2개 더 넣어서 8개로 해도 괜찮을 듯 싶어요ㅎㅎ
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
@@ -10,34 +9,38 @@ import javax.swing.table.DefaultTableCellRenderer;
 
 
 public class EwhaSchoolFood extends JFrame {
-	int count = 0;
 	String show = "";
 	private TextField[] numb;
 	TextArea ta;
 	private DefaultTableModel paymentModel;
+	private int [] count= {0,0,0,0,0,0,0,0};
 	
 	public EwhaSchoolFood() {
         
 		JFrame frame = new JFrame("EWHA 분식점");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(0, 0, 625, 1000);
 		frame.setBackground(new Color(204, 255, 204)); //Frame
+
  
 		Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 17);
-		Font font1 = new Font(Font.MONOSPACED, Font.BOLD, 22); //Font
+		Font font1 = new Font(Font.MONOSPACED, Font.BOLD, 23); //Font
 
-		Label highbar = new Label();
-		highbar.setText("EWHA 분식점");
+		Label highbar = new Label();//ewha 분식점 label
+		highbar.setText(" EWHA 분식점 ");
 		highbar.setFont(font1);
+		highbar.setBounds(230,10,170,30);
+		highbar.setBackground(Color.pink);
 		
-		Panel pNorth = new Panel();
+		Panel pNorth = new Panel(); 
 		pNorth.setBackground(Color.LIGHT_GRAY);
 		pNorth.setLayout(null);
 		pNorth.setSize(0, 500);
-		pNorth.setFont(font); //North Panel
-		pNorth.add(highbar);
+		pNorth.setFont(font); 
+		pNorth.add(highbar);//North Panel
 		
-		String menu [] = {"떡볶이", "라면", "우동", "김밥", "쫄면", "냉면"};
-		int price[] = {3000, 1500, 2000, 2500, 2500, 2500};
+		String menu [] = {"떡볶이", "라면", "우동", "김밥", "쫄면", "냉면", "콜라", "사이다"};
+		int price[] = {3500, 2000, 3000, 2500, 3000, 3500, 1500, 1500};
 		JButton bt[] = new JButton[menu.length];
 		TextField numb[] = new TextField[menu.length];
 		Button minus[] = new Button[menu.length];
@@ -46,15 +49,26 @@ public class EwhaSchoolFood extends JFrame {
 		Label l[] = new Label[menu.length];
 		ImageIcon icon[] = new ImageIcon[menu.length];
  
-        String imagePath = "C:/EwhaKiosk/";
-
+        String imagePath = "C:\\EwhaKiosk\\";
+        
+        //메뉴 이름, 메뉴 위치 정하기
+        JLabel menuname []= new JLabel [8];
+        for(int i=0;i<8;i++) {
+        	menuname[i]= new JLabel();
+        	menuname[i].setText(menu[i]);
+        	menuname[i].setBounds(60 + i * 150, 40, 70, 20); 
+        	
+        }
+        
+        
         for (int i = 0; i < menu.length; i++) {
 
             bt[i] = new JButton(menu[i]);
             if (i < 4) {
-                bt[i].setBounds(25 + i * 150, 50, 100, 100);
-            } else {
-                bt[i].setBounds(25 + (i - 4) * 150, 300, 100, 100);
+            	bt[i].setBounds(25 + i * 150, 60, 100, 100);
+            } 
+            else {
+                bt[i].setBounds(25 + (i - 4) * 150, 290, 100, 100);
             }
             
             File imgFile = new File(imagePath + i + ".png");
@@ -62,29 +76,32 @@ public class EwhaSchoolFood extends JFrame {
                 icon[i] = new ImageIcon(imgFile.getAbsolutePath());
                 bt[i].setIcon(icon[i]);
             } else {
-                // 파일이 존재하지 않는 경우 처리
+                // 파일이 존재하지 않는 경우
                 System.out.println("이미지 파일이 존재하지 않습니다: " + imgFile.getAbsolutePath());
             }
 			numb[i] = new TextField("0"); // 초기값 0으로 설정
 			numb[i].setBackground(Color.white);
 			numb[i].setEditable(false);
-			numb[i].setBounds(bt[i].getX() + 30, bt[i].getY() + 105, 40, 20);
+			numb[i].setBounds(bt[i].getX() + 30, bt[i].getY() + 130, 40, 20);
 			
 			minus[i] = new Button("-");
 			minus[i].setBounds(bt[i].getX(), numb[i].getY(), 20, 20);
-			minus[i].setEnabled(false); //Minus button
+			minus[i].setEnabled(true); //Minus button
  
 			plus[i] = new Button("+");
 			plus[i].setBounds(bt[i].getX() + (100 - 20), numb[i].getY(), 20, 20);
-			plus[i].setEnabled(false); // Plus button
+			plus[i].setEnabled(true); // Plus button
  
-			l[i] = new Label(price[i] + "원");
+			l[i] = new Label(price[i] + " 원");
 			l[i].setBounds(bt[i].getX() + 20, numb[i].getY() - 25, 100, 20); //price button
  
 			ok[i] = new JButton("확인");
 			ok[i].setBounds(bt[i].getX(), numb[i].getY() + 30, 100, 20);
 			ok[i].setEnabled(true); //확인 button
  
+			menuname[i].setBounds(bt[i].getX() + 30, bt[i].getY() + -20, 100, 20);
+			
+			pNorth.add(menuname[i]);
 			pNorth.add(bt[i]);
 			pNorth.add(numb[i]);
 			pNorth.add(minus[i]);
@@ -99,6 +116,7 @@ public class EwhaSchoolFood extends JFrame {
         ta.setEditable(false);
         ta.setFont(font1); //Center
 
+
         Panel pSouth = new Panel();
         pSouth.setFont(font);
         pSouth.setBackground(new Color(255, 255, 215)); //South
@@ -108,13 +126,25 @@ public class EwhaSchoolFood extends JFrame {
         pSouth.add(bt1);
         pSouth.add(bt2);
  
+        //확인버튼 눌렀을 때
        bt1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 showPayment();
             }
         });
-        
-        
+       
+       //초기화버튼 눌렀을 때
+       bt2.addActionListener(new ActionListener() {
+    	   public void actionPerformed(ActionEvent e) {
+    		   for(int i=0;i<6;i++) {
+    			   count[i]=0;
+    			   numb[i].setText("0");
+    			   ok[i].setEnabled(true);
+    		   }    		   
+    		   ta.setText("  메뉴명        단가        수량        합계\n\n");
+    	   }
+       });
+       
         frame.add(pNorth, BorderLayout.NORTH);
         frame.add(ta, BorderLayout.CENTER);
         frame.add(pSouth, BorderLayout.SOUTH);
@@ -131,8 +161,7 @@ public class EwhaSchoolFood extends JFrame {
                     plus[j].setEnabled(true);
                     bt[j].setEnabled(false);
                     ok[j].setEnabled(false);
- 
-                    count = 0;
+                    
                 }
             }); //Order event
  
@@ -140,12 +169,13 @@ public class EwhaSchoolFood extends JFrame {
             plus[i].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    count = count + 1;
-                    numb[j].setText(Integer.toString(count)); // 수량 증가 후 TextField에 반영
+                	count[j]++;
+                    numb[j].setText(Integer.toString(count[j])); // 수량 증가 후 TextField에 반영
                     ok[j].setEnabled(true); // 수량이 변경되었으므로 확인 버튼 활성화
-                    if (count > 0) {
+                    if (count[j] > 0) {
                         minus[j].setEnabled(true);
                     }
+                    
                 }
             });
 
@@ -153,24 +183,30 @@ public class EwhaSchoolFood extends JFrame {
             minus[i].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (count > 0) {
-                        count = count - 1;
-                        numb[j].setText(Integer.toString(count)); // 수량 감소 후 TextField에 반영
+                    if (count[j] > 0) {
+                    	count[j]--;
+                        numb[j].setText(Integer.toString(count[j])); // 수량 감소 후 TextField에 반영
                         ok[j].setEnabled(true); // 수량이 변경되었으므로 확인 버튼 활성화
                     }
-                    if (count == 0) {
+                    if (count[j] == 0) {
                         minus[j].setEnabled(false);
                     }
+                    
                 }
             });
 
-            // Confirm (ok) button event
+            
             ok[i].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    show = bt[j].getActionCommand();
-                    ta.append("   " + show + "       " + price[j] + "        " + count + "         " + price[j]*count
-                            + "원" + "\n");
+                	if (menu[j].length() == 3) {
+                		 ta. append("  " + menu[j] + "        " + price[j] + "        " + count[j] + "         " + price[j]*count[j]
+                                 + "원" + "\n");
+                	} else {
+                		ta. append("  " + menu[j] + "          " + price[j] + "        " + count[j] + "         " + price[j]*count[j]
+                                + "원" + "\n");
+                	}
+                    
                     ok[j].setEnabled(false); // 주문 후에는 버튼 비활성화
                 }
             });
@@ -188,6 +224,7 @@ public class EwhaSchoolFood extends JFrame {
 	//결제창
 		public void showPayment() {
 	        JFrame payment = new JFrame();
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        payment.setTitle("결제");
 	        payment.setLayout(null);
 	        JLabel title= new JLabel("결제목록");
@@ -206,6 +243,8 @@ public class EwhaSchoolFood extends JFrame {
 	        paymentTable.getColumnModel().getColumn(0).setPreferredWidth(180);
 	        paymentTable.getColumnModel().getColumn(1).setPreferredWidth(180); 
 	        paymentTable.getColumnModel().getColumn(2).setPreferredWidth(180);
+	        paymentTable.setOpaque(true);
+	        paymentTable.setBackground(Color.white);
 	        
 	       //글자 가운데정렬
 	        DefaultTableCellRenderer centeralign= new DefaultTableCellRenderer();
@@ -215,9 +254,13 @@ public class EwhaSchoolFood extends JFrame {
 	        }
 	        
 	        JScrollPane paymentScrollPane = new JScrollPane(paymentTable);
-
+	        paymentScrollPane.setBorder(BorderFactory.createEmptyBorder());
+	        paymentTable.setBorder(BorderFactory.createEmptyBorder()); //테이블 가장 바깥쪽 테두리를 빈값으로 만듦 -> 없어짐	        
+	        
 	        //총 금액, 결제버튼 설정
 	        JLabel paymentTotalLabel = new JLabel("총 금액: "+totalprice()+" 원", SwingConstants.CENTER);
+	        paymentTotalLabel.setOpaque(true);
+	        paymentTotalLabel.setBackground(Color.pink);
 	        Font totalcurrentfont= paymentTotalLabel.getFont();
 	        Font totalnewfont=new Font(totalcurrentfont.getName(), totalcurrentfont.getStyle(),25);
 	        paymentTotalLabel.setFont(totalnewfont);
@@ -254,7 +297,7 @@ public class EwhaSchoolFood extends JFrame {
 	        for (String line : lines) {
 	            if (!line.trim().isEmpty() && !line.startsWith("  메뉴명")) {
 	                String[] parts = line.trim().split("\\s+");
-	                if (parts.length >= 4) {
+	                if (parts.length == 4) {
 	                    String name = parts[0];
 	                    String price = parts[1];
 	                    String count = parts[2];
@@ -270,9 +313,9 @@ public class EwhaSchoolFood extends JFrame {
 	        for (String line : lines) {
 	            if (!line.trim().isEmpty() && !line.startsWith("  메뉴명")) {
 	                String[] parts = line.trim().split("\\s+");
-	                if (parts.length >= 4) {
+	                if (parts.length == 4) {
 	                    String total = parts[3].replace("원", "");
-	                    totalprice= Integer.parseInt(total); 
+	                    totalprice+= Integer.parseInt(total); 
 	                }
 	            }
 	        }
